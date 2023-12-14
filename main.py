34fea1c587281541ae78a2402f77d772ef9bcc5c
Dax-000 <CiderBinderBunny@yandex.ru>
2023-12-14 14:10:23 +0300

    Добавить цепочку переходов на сайт Tensor.ru

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome()
 driver.get("https://sbis.ru/")
+contacts = driver.find_element(By.LINK_TEXT, "Контакты")
+contacts.click()
+tensor = driver.find_element(By.XPATH, "//a[@title='tensor.ru']")
+tensor.click()
+
 driver.close()
+
