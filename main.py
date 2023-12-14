778930a8144bc64893f4f28cb871c97de8e69343
Dax-000 <CiderBinderBunny@yandex.ru>
2023-12-14 22:04:45 +0300

    Добавить переход в about блока Сильные люди

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 
 driver = webdriver.Chrome()
 driver.get("https://sbis.ru/")
 contacts = driver.find_element(By.LINK_TEXT, "Контакты")
 contacts.click()
+
 tensor = driver.find_element(By.XPATH, "//a[@title='tensor.ru']")
+assert tensor.get_attribute("href") == "https://tensor.ru/"
 tensor.click()
+sleep(2.5)
+
+driver.switch_to.window(driver.window_handles[1])
+block4_text = driver.find_element(By.XPATH, "//p[text()='Сила в людях']")
+block4_about = driver.find_element(By.XPATH, "//p[text()='Сила в людях']//following::a[text()='Подробнее']")
+assert block4_about.get_attribute("href") == "https://tensor.ru/about"
+driver.execute_script("arguments[0].click();", block4_about)    # ссылка делала мозг с обычным кликом
+
 
 driver.close()
 
