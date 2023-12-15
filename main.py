c85337c94039abc05308be9619f4b53b3f46413a
Dax-000 <CiderBinderBunny@yandex.ru>
2023-12-15 14:45:09 +0300

    Переделать main.py под pytest с использование pageObject паттерна

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import sleep
+from PageSbisIndex import SbisIndex
+from PageSbisContacts import SbisContacts
+from PageTensorIndex import TensorIndex
+from PageTensorAbout import TensorAbout
 
 
-UP_TO_SECTION = "ancestor::div[contains(@class, 'tensor_ru-container tensor_ru-section')]"
+def test_go_to_contacts(browser):
+    sbis_index = SbisIndex(browser)
+    sbis_index.go_to_site("https://sbis.ru/")
+    sbis_index.click_on_contacts()
+    assert "https://sbis.ru/contacts" in sbis_index.get_url()
 
 
-def sizes_is_equal(*web_elements):
-    return len({tuple(e.size.values()) for e in web_elements}) == 1
+def test_go_to_tensor(browser):
+    sbis_contacts = SbisContacts(browser)
+    sbis_contacts.click_on_tensor()
+    assert sbis_contacts.get_url() == "https://tensor.ru/"
 
 
-driver = webdriver.Chrome()
-driver.get("https://sbis.ru/")
-contacts = driver.find_element(By.LINK_TEXT, "Контакты")
-contacts.click()
+def test_sila_section(browser):
+    tensor_index = TensorIndex(browser)
+    assert tensor_index.check_sila_section()
+    tensor_index.click_on_about()
+    assert tensor_index.get_url() == "https://tensor.ru/about"
 
-tensor = driver.find_element(By.XPATH, "//a[@title='tensor.ru']")
-assert tensor.get_attribute("href") == "https://tensor.ru/"
-tensor.click()
-sleep(2.5)
 
-driver.switch_to.window(driver.window_handles[1])
-block4_section = driver.find_element(By.XPATH, f"//p[text()='Сила в людях']//{UP_TO_SECTION}")
-block4_about = driver.find_element(By.XPATH, "//p[text()='Сила в людях']//following::a[text()='Подробнее']")
-assert block4_about.get_attribute("href") == "https://tensor.ru/about"
-driver.execute_script("arguments[0].click();", block4_about)    # ссылка делала мозг с обычным кликом
-
-# Секция "Работаем"
-image_wrappers = driver.find_elements(By.XPATH, f"//h2[text()='Работаем']//{UP_TO_SECTION}//img/..")
-assert sizes_is_equal(*image_wrappers)
-
-driver.close()
+def test_rabota_section(browser):
+    tensor_about = TensorAbout(browser)
+    assert tensor_about.check_size_equality()
